e1547236f299ab9af2b175a49cca39397288c136
Shreyas Pangal <shreyaspangal03@gmail.com>
2024-01-14 18:43:46 +0530

    feat: login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import CustomizedInput from '../components/shared/CustomizedInput'
+import { LuLogIn as LoginIcon } from "react-icons/lu";
+import toast from 'react-hot-toast';
+import { useAuth } from '../context/AuthContext';
 
 const Login = () => {
+    const auth = useAuth();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+
+        try {
+            toast.loading("Signing In", { id: "login" })
+            await auth?.login(email, password);
+            toast.success("Signed In Successfully", { id: "login" })
+        } catch (error: any) {
+            toast.error("Signing In Failed", { id: "login" });
+            console.log(error);
+        }
+    }
+
     return (
-        <div>Login</div>
+        <Box width={"100%"} height={"100%"} display={'flex'} flex={1}>
+            <Box padding={2} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+                <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+            </Box>
+            <Box display={'flex'} flex={{ xs: 1, md: 0.5 }} justifyContent={'center'} alignItems={'center'} padding={2} ml={'auto'} mt={16}>
+                <form
+                    onSubmit={handleSubmit}
+                    style={{
+                        margin: "auto",
+                        padding: "30px",
+                        boxShadow: "10px 10px 20px #000",
+                        borderRadius: "10px",
+                        border: "none"
+                    }}>
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center"
+                    }}>
+                        <Typography variant="h4" textAlign={'center'} padding={2} fontWeight={600}>
+                            Login
+                        </Typography>
+                        <CustomizedInput name="email" type='email' label='Email' />
+                        <CustomizedInput name="password" type='password' label='Password' />
+                    </Box>
+                    <Button
+                        type='submit'
+                        sx={{
+                            px: 2,
+                            py: 1,
+                            mt: 2,
+                            width: "400px",
+                            borderRadius: 2,
+                            backgroundColor: "#00fffc",
+                            ":hover": {
+                                backgroundColor: "white",
+                                color: "black"
+                            }
+                        }}
+                        endIcon={<LoginIcon />}
+                    >
+                        Login
+                    </Button>
+                </form>
+            </Box>
+        </Box>
     )
 }
 
