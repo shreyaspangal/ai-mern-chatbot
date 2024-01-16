b52b9f83b4819ebf2811d3075d064a18bd998939
Shreyas Pangal <shreyaspangal03@gmail.com>
2024-01-16 18:47:01 +0530

    feat & ui: singup

--- frontend/src/helpers/api-communicators.ts
+++ frontend/src/helpers/api-communicators.ts
@@ -10,6 +10,16 @@ export const loginUser = async (email: string, password: string) => {
     return data;
 }
 
+export const signupUser = async (name: string, email: string, password: string) => {
+    const res = await axios.post("/user/signup", { name, email, password });
+    if (res.status !== 201) {
+        throw new Error('Unable to signup');
+    }
+
+    const data = res.data;
+    return data;
+}
+
 export const logoutUser = async () => {
     const res = await axios.get("/user/logout");
     if (res.status !== 200) {
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import CustomizedInput from '../components/shared/CustomizedInput'
+import toast from 'react-hot-toast';
+import { useAuth } from '../context/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const auth = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (auth?.user) {
+            return navigate("/chat");
+        }
+    }, [auth])
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const name = formData.get('name') as string;
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+
+        if (!email || !password) {
+           return toast.error("All fields are required!");
+        }
+
+        try {
+            toast.loading("Signing In", { id: "signin" })
+            await auth?.signup(name, email, password);
+            toast.success("Signed In Successfully", { id: "signin" })
+        } catch (error: any) {
+            toast.error("Signing In Failed", { id: "signin" });
+            console.log(error);
+        }
+    }
+
     return (
-        <div>Signup</div>
+        <div className="flex w-full h-full flex-1 items-center">
+            <div className="hidden lg:flex shrink-0 p-[16px] mt-[64px]">
+                <img src="airobot.png" alt="Robot" className='w-[400px]' />
+            </div>
+            <div className="flex flex-1 md:flex-[0.5] p-[16px] mx-auto lg:ml-auto mt-[128px] lg:mt-[50px]">
+                <form
+                    onSubmit={handleSubmit}
+                    className='m-auto p-[30px] shadow-2xl shadow-gray-700 rounded-[15px] border-none'>
+                    <div className="flex flex-col justify-center gap-7">
+                        <h4 className="text-center p-2 font-semibold text-3xl">Signup</h4>
+                        <CustomizedInput name="name" type='text' label='Name' placeholder='Name' />
+                        <CustomizedInput name="email" type='email' label='Email' placeholder='Email' />
+                        <CustomizedInput name="password" type='password' label='Password' placeholder='Password' />
+                    </div>
+                    <button className="button mt-5 px-2 py-3 w-[400px] rounded-md bg-blue-600 hover:bg-white active:bg-white/50 hover:text-gray-900 font-semibold" type="submit">
+                        Signup
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
-export default Signup
+export default Signup;
